824983f937f685f43032842d5dc2ee9fbaf19805
KamilGaribov <kamilgarib@gmail.com>
2021-11-23 15:18:15 +0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 // export const apiUrl = process.env.REACT_APP_API_URL;
-export const apiUrl = "http://localhost:8000/";
-export const perPage = 3;
+// export const apiUrl = "http://localhost:8000/";
+export const apiUrl = "https://panel.novamed.az/;
+export const perPage = 10;
